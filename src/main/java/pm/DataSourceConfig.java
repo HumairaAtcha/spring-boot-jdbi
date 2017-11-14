22900e1ef614f1b78fb0b2145a8f6d70a394524f
Philippa Main <philippa.main@autotrader.co.uk>
2017-11-14 07:37:30 +0000

    Moved data source configuration to application.yml.

--- src/main/java/pm/DataSourceConfig.java
+++ src/main/java/pm/DataSourceConfig.java
@@ -3,6 +3,7 @@ package pm;
 import com.zaxxer.hikari.HikariDataSource;
 import org.skife.jdbi.v2.DBI;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pm.animal.repository.JdbiSqlRepository;
@@ -13,21 +14,35 @@ import java.sql.SQLException;
 @Configuration
 public class DataSourceConfig {
 
+    @Value("${datasource.driver}")
+    private String driver;
+
+    @Value("${datasource.url}")
+    private String jdbcUrl;
+
+    @Value("${datasource.user}")
+    private String username;
+
+    @Value("${datasource.password}")
+    private String password;
+
     @Bean
     @Qualifier("Hikari")
     public DataSource hikariDataSource() {
         HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setJdbcUrl("jdbc:mysql://localhost/zoo");
+        dataSource.setDriverClassName(driver);
+        dataSource.setJdbcUrl(jdbcUrl);
 
-        dataSource.setUsername("root");
-        dataSource.setPassword("password");
+        username = "root";
+        dataSource.setUsername(username);
+        password = "password";
+        dataSource.setPassword(password);
 
         return dataSource;
     }
 
     @Bean
-    JdbiSqlRepository jdbiSqlRepository(@Qualifier("Hikari") DataSource dataSource) throws SQLException { //TODO: shouldn't need qualifier - get rid of spring-jdbc dependency
+    JdbiSqlRepository jdbiSqlRepository(@Qualifier("Hikari") DataSource dataSource) throws SQLException { // TODO: shouldn't need qualifier - get rid of spring-jdbc dependency
         DBI dbi = new DBI(dataSource);
         return dbi.onDemand(JdbiSqlRepository.class);
     }
